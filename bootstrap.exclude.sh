6d4957f6f6f7ea62f97bcf757535fae252c6de3f
abstractmachines <abstractmachines@gmail.com>
2020-08-17 14:36:20 -0700

    Basic symlink script done but TODO figure out excludes dirs and greps.

--- bootstrap.exclude.sh
+++ bootstrap.exclude.sh
@@ -7,11 +7,16 @@ init () {
 	echo "$PROMPT Let's bootstrap your machine."
 }
 
+# man ln: make links. ln w opt -s makes symlinks; w/ opt v, verbosely.
 symlinx () {
     echo "$PROMPT \n\n ** Symlinking your dotfiles repo to your user directory. ** \n\n Proceed? (y/n)"
 	read inp
 	if [ "$inp" = 'y' -o "$inp" = 'Y' ] ; then
         echo "okay dude"
+        for file in $( ls -A | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md|LICENSE|terminal' ) ; do
+			ln -sv "$PWD/$file" "$HOME"
+		done
+
     else
         echo "never mind"
     fi
