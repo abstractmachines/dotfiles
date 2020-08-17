50e90eff09ebf85bb3fe498e01bf882d530c8734
abstractmachines <abstractmachines@gmail.com>
2020-08-17 14:16:13 -0700

    Basic loop.

--- bootstrap.exclude.sh
+++ bootstrap.exclude.sh
@@ -5,8 +5,18 @@ PROMPT='abstractmachines-bootstrap'
 init () {
 
 	echo "$PROMPT Let's bootstrap your machine."
-	ls -al
-	pwd
+}
+
+symlinx () {
+    echo "$PROMPT \n\n ** Symlinking your dotfiles repo to your user directory. ** \n\n Proceed? (y/n)"
+	read inp
+	if [ "$inp" = 'y' -o "$inp" = 'Y' ] ; then
+        echo "okay dude"
+    else
+        echo "never mind"
+    fi
 }
 
 init
+symlinx
+# brewinstall
