5e2fc21ba7827edb1ade6ae69b8a1ca36f42fb8b
abstractmachines <abstractmachines@gmail.com>
2020-08-30 17:29:40 -0700

    Improving bash scripts, fun stuff.

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,8 +21,31 @@ hi () {
 
 init () {
     hi
-    echo "$PROMPT Let's bootstrap your machine."
+    echo "$PROMPT Let's bootstrap your machine!"
+
+    if proceed; then
+        symlinx
+    fi
 }
+
+noAction () {
+    echo " * No action taken. *"
+}
+
+proceed () {
+    echo "\n** Proceed? (y/n) **"
+    read yOrN
+    if [ "$yOrN" = 'y' -o "$yOrN" = 'Y' ] ; then
+        return
+    else 
+        noAction
+    fi
+    # if [[ ""]]
+
+    false
+}
+
+
 
 # man ln: make links. ln w opt -s makes symlinks; w/ opt v, verbosely.
 symlinx () {
@@ -34,7 +57,7 @@ symlinx () {
         ln -sv "$PWD/.bash_profile" "$HOME/.bash_profile"
 
     else
-        echo "\n* no action taken ...Or something went wrong during symlinx(). *"
+        noAction
     fi
 }
 
@@ -44,15 +67,18 @@ symlinx () {
 # printing out the operating system and [grepping for](https://man7.org/linux/man-pages/man1/grep.1.html)
 # the darwin OS.
 brewinstall () {
-    if [ echo $OSTYPE | grep darwin ] ; then
+    if [ $( echo $OSTYPE | grep 'darwin' ) ] ; then
         echo "Using OSX, and installing homebrew package manager. \n\n Continue? Y/N."
         read inp
+        if [ "$inp" = 'y' -o "$inp" = 'Y' ] ; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+        fi
     else
-        echo "Nevermind, you're not on Mac OS."
+        echo " * You're not on Mac/OSX. * "
     fi
 }
 
 init
-symlinx
+# symlinx
 # brewinstall
 # etc
