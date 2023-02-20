a63549c36818af89dc3eb13cf5f5794a48bd31c4
Amanda Falke <afalke@igalia.com>
2023-02-20 13:05:37 -0500

    rpi3 scripts

--- rpi3-coggy.sh
+++ rpi3-coggy.sh
@@ -5,14 +5,14 @@
 # sh rpi3-coggy.sh
 
 echo Enter RPI IP: 
-read $ip
+read ip
 export RPI_IP=$ip # rpi
 export WEBKIT_INSPECTOR_HTTP_SERVER="$RPI_IP:9224"
 export WPE_BCMRPI_TOUCH=1
 export WPE_BCMRPI_CURSOR=1
 
 echo Enter target: 
-read $target
+read target
 export TARGET=$target
 
 echo Env vars are:
