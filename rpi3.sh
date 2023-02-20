a709fed43b6fd565a39dced0aab029a499481bd6
Amanda Falke <afalke@igalia.com>
2023-02-20 12:18:49 -0500

    rpi3 scripts

--- rpi3.sh
+++ rpi3.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 echo "enter RPI IP"
-read $ip
+read ip
+echo IP: $ip
+
 echo "enter RPI usr"
-read $usr
+read usr
+echo usr: $usr
+
 export RPI_IP=$ip
 export RPI_USR=$usr
 
+echo "ssh: $RPI_USR@$RPI_IP ..."
+
 # ssh-keygen -R $RPI_IP
-# sshpass -p $RPI_PW ssh $RPI_USR@$RPI_IP
 
 ssh $RPI_USR@$RPI_IP
